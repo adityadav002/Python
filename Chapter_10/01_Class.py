2ce87c04fb9439edd204ff26399f2bc9e48da58f
Aditya Yadav <166471833+adityadav002@users.noreply.github.com>
2025-09-01 22:19:53 +0530

    Delete Chapter_10 directory

--- Chapter_10/01_Class.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    language = 'Python'  # class attribute
-    salary = 4000000
-
-details = Employee()
-details.name = "Aditya" # instance attributes , more preference than class attribute
-details.language = "React"
-print(details.name, details.language, details.salary)
